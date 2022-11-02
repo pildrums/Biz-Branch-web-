edae01234990ac47ceb6c6e7445d535d810d56f9
Pildrums <there7788@gmail.com>
2022-11-02 16:16:17 +0900

    Practice project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const todos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
+  const onDragEnd = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Container>
+        <Boards>
+          <Droppable droppableId="one">
+            {(drop) => (
+              <Board ref={drop.innerRef} {...drop.droppableProps}>
+                {todos.map((todo, index) => (
+                  <Draggable draggableId={todo} index={index}>
+                    {(drag) => (
+                      <Card
+                        ref={drag.innerRef}
+                        {...drag.dragHandleProps}
+                        {...drag.draggableProps}
+                      >
+                        {todo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {drop.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Container>
+    </DragDropContext>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  background: ${(props) => props.theme.boardColor};
+  color: ${(props) => props.theme.textColor};
+  padding-top: 30px;
+  padding: 20px 10px;
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  background: ${(props) => props.theme.cardColor};
+  color: ${(props) => props.theme.textColor};
+  padding: 5px 10px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+`;
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#E8EDE7",
+  boardColor: "#81BECE",
+  cardColor: "#81BECE",
+  textColor: "#333",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#012E4A",
+  boardColor: "#036280",
+  cardColor: "#378BA4",
+  textColor: "#fff",
+};
